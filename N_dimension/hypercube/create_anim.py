23dc5253a533e24852f2beac091708bbe12806d3
masterdesky <masterdesky@gmail.com>
2021-04-10 00:35:49 +0200

    added nd hypercube anim
    Former-commit-id: 7b76dc0412d8dff8654088bea13f8329b8bf1d99

---
+++ N_dimension/hypercube/create_anim.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import imageio
+from datetime import datetime
+
+path = './output/'
+out = './'
+
+# Gather path to appropriate images to concatenate
+print('[{0}] Reading in list of images to concatenate'.format(datetime.now().isoformat()), file=sys.stderr)
+file_list = []
+for file in os.listdir(path):
+    if "frame" in file:
+        complete_path = path + file
+        file_list.append(complete_path)
+# Sort filenames
+file_list.sort(key=lambda x: int(''.join(filter(str.isdigit, x))))
+
+# Define writer and create video
+writer = imageio.get_writer(out + 'hypercube_rotate.mp4', fps=30)
+
+for i, im in enumerate(file_list):
+    text = '[{0}] Appending image #{1}...'.format(datetime.now().isoformat(), i)
+    print(text, file=sys.stderr)
+    writer.append_data(imageio.imread(im))
+writer.close()
